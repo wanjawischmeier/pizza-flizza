8198526d766d2fa06c87b2449c4ee4f93d4764e0
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2023-03-21 01:06:04 +0100

    implemented grid view adapter
    - ported to using grid view adapter
    - added penny items to server template
    - imported majority of product images
    - test custom icon

--- app/src/main/java/com/wanjawischmeier/pizza/OrderFragment.kt
+++ app/src/main/java/com/wanjawischmeier/pizza/OrderFragment.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.GridView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.gms.tasks.Task
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.round
 
 
@@ -16,7 +19,8 @@ const val GROUP_ID = "prenski_12"
 const val SHOP_ID = "penny_burgtor"
 
 class OrderFragment : CallableFragment() {
-    private var itemChildren = hashMapOf<View, String>()
+    private lateinit var itemGridViewAdapter: ItemGridViewAdapter
+    private lateinit var itemsGrid: GridView
     private lateinit var priceView: TextView
     private lateinit var main: MainActivity
     private lateinit var order: Order
@@ -34,22 +38,39 @@ class OrderFragment : CallableFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         bottomLayoutVisible = false
+        itemsGrid = view.findViewById(R.id.items_grid)
+
+        main.swipeRefreshLayout.setOnChildScrollUpCallback { _, _ ->
+            itemsGrid.canScrollVertically(-1)
+        }
     }
 
+    @SuppressLint("DiscouragedApi")
     override fun onShow(): Task<Unit> {
         topBubbleVisible = true
 
+        val itemArrayList: ArrayList<ItemModel> = ArrayList<ItemModel>()
+        val items = main.shop.items
+
+        total = 0f
+
         return User.getUser(GROUP_ID, main.userId).continueWith {
             order = it.result.orders[SHOP_ID] ?: hashMapOf()
-            inflateItems()
 
-            total = 0f
+            // TODO: manage context being null
+            itemGridViewAdapter = ItemGridViewAdapter(context!!, itemArrayList)
+            itemsGrid.adapter = itemGridViewAdapter
 
-            for ((view, itemId) in itemChildren) {
-                val price = main.shop.items[itemId]?.price ?: 0f
+            for ((itemId, item) in items) {
+                if (itemGridViewAdapter.contains(itemId)) continue
                 val count = order[itemId]?.get(ITEM_COUNT) ?: 0L
-                view.findViewById<TextView>(R.id.order_count).text = count.toString()
-                total = round((total + price * count) * 100) / 100
+                total = round((total + item.price * count) * 100) / 100
+
+                val imageId = resources.getIdentifier(itemId, "drawable", context!!.packageName)
+                itemArrayList.add(ItemModel(
+                    itemId, item.name, item.price, count,
+                    if (imageId == 0) R.drawable.baeckerkroenung else imageId
+                ))
             }
 
             priceView.text = getString(R.string.price_format).format(total)
@@ -60,40 +81,19 @@ class OrderFragment : CallableFragment() {
         // TODO: Not yet implemented
     }
 
-    @SuppressLint("InflateParams")
-    private fun inflateItems() {
-        val itemsList = view?.findViewById<LinearLayout>(R.id.items_list)
-
-        val items = main.shop.items
-        for ((itemId, item) in items) {
-            if (itemChildren.values.contains(itemId)) continue
-
-            // Avoid parralel inflations
-            activity?.runOnUiThread {
-                val inflated = layoutInflater.inflate(R.layout.card_order, null)
-
-                inflated.findViewById<TextView>(R.id.order_name).text = item.name
-                inflated.findViewById<TextView>(R.id.order_price).text = getString(R.string.price_format).format(item.price)
-
-                itemChildren += inflated to itemId
-                itemsList?.addView(inflated)
-            }
-        }
-    }
-
     fun modifyCount(view: View, change: Long) {
-        val countView = (view.parent as ViewGroup).findViewById<TextView>(R.id.order_count)
-        val count = countView.text.toString().toLong() + change
-        if (count < 0 || count > 99) return
-        countView.text = count.toString()
-
-        val itemId = itemChildren[view.parent.parent as View] ?: return
+        val parent = view.parent.parent.parent as ConstraintLayout
+        val itemModel = itemGridViewAdapter.getItemByView(parent) ?: return
+        val itemId = itemModel.id
         val item = main.shop.items[itemId] ?: return
-        total = round((total + item.price * change) * 100) / 100
+
+        itemModel.count = max(0, min(99, itemModel.count + change))
+        total = max(0f, min(99f, round((total + item.price * change) * 100) / 100))
         priceView.text = getString(R.string.price_format).format(total)
         bottomLayoutVisible = total > 0
 
-        order[itemId] = listOf(count, Calendar.getInstance().time.time)
+        order[itemId] = listOf(itemModel.count, Calendar.getInstance().time.time)
+        itemGridViewAdapter.notifyDataSetChanged()
     }
 
     fun placeOrder() {
