e7b53ebaaf8c7039409507782e94ac80983e7b8e
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2023-04-29 20:04:38 +0200

    added login function

--- flutter-implementation/pizza_flizza/lib/pages/login_page.dart
+++ flutter-implementation/pizza_flizza/lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_flizza/custom_icons.dart';
 import 'package:pizza_flizza/theme.dart';
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _isLoading = false;
+  String _email = '';
+  String _password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,99 +23,119 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text("Login Page"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Column(children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 40),
-              child: Center(
-                child: Container(
-                  width: 200,
-                  height: 150,
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Themes.grayMid,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: const Offset(2, 2),
-                      )
-                    ],
-                  ),
-                  child: const FittedBox(
-                    child: Icon(
-                      PizzaIcons.logo,
-                      color: Themes.cream,
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Center(
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Themes.grayMid,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: const Offset(2, 2),
+                        )
+                      ],
+                    ),
+                    child: const FittedBox(
+                      child: Icon(
+                        PizzaIcons.logo,
+                        color: Themes.cream,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com'),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: 'Enter valid email id as abc@gmail.com'),
+                  textInputAction: TextInputAction.next,
+                  onChanged: (value) => _email = value,
+                ),
               ),
-            ),
-            const Padding(
-              padding:
-                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15, bottom: 0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter secure password'),
+                  textInputAction: TextInputAction.done,
+                  onChanged: (value) => _password = value,
+                  onSubmitted: (value) => _signIn(),
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                // TODO: FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: _isLoading ? Colors.lightBlueAccent : Colors.blue,
-                  borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
+              TextButton(
                 onPressed: () {
-                  setState(() {
-                    _isLoading = true;
-                  });
+                  // TODO: FORGOT PASSWORD SCREEN GOES HERE
                 },
-                child: _isLoading
-                    ? const SizedBox(
-                        height: 25,
-                        width: 25,
-                        child: CircularProgressIndicator(
-                          color: Colors.white,
-                          strokeWidth: 3,
-                        ),
-                      )
-                    : const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white, fontSize: 25),
-                      ),
+                child: const Text(
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.blue, fontSize: 15),
+                ),
               ),
-            ),
-          ]),
-          const GoogleSignInButton(),
-        ],
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: _isLoading ? Colors.lightBlueAccent : Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: _signIn,
+                  child: _isLoading
+                      ? const SizedBox(
+                          height: 25,
+                          width: 25,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                            strokeWidth: 3,
+                          ),
+                        )
+                      : const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                ),
+              ),
+            ]),
+            const GoogleSignInButton(),
+          ],
+        ),
       ),
     );
+  }
+
+  void _signIn() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: _email, password: _password);
+    } catch (error) {
+      print('error');
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 }
